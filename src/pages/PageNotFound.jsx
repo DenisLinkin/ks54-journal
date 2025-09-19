64d3cebb103fffbbeee96cd94e1a4e692e8dee9b
Denis Linkin <linkin_da@citis.ru>
2025-09-19 12:57:00 +0300

    Добавил стриницу 404

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,3 +1,30 @@
+import { Button, Container, Group, Text, Title } from "@mantine/core";
+import classes from "../ui/PageNotFound.module.css";
+import { Illustration } from "../ui/Illustration";
+
 export default function PageNotFound() {
-  return <div>PageNotFound</div>;
+  return (
+    <Container className={classes.root}>
+      <div className={classes.inner}>
+        <Illustration />
+        <div className={classes.content}>
+          <Title className={classes.title}>Здесь ничего нет</Title>
+          <Text
+            c="dimmed"
+            size="lg"
+            ta="center"
+            className={classes.description}
+          >
+            Страница, которую вы пытаетесь открыть, не существует. Возможно, вы
+            неправильно набрали текст адрес или страница были перемещены на
+            другой URL-адрес. Если вы считаете , что это ошибка, обратитесь в
+            службу поддержки.
+          </Text>
+          <Group justify="center">
+            <Button size="md">Вернуться назад</Button>
+          </Group>
+        </div>
+      </div>
+    </Container>
+  );
 }
